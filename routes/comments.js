b0377f5145185ccfe5bc17a6aa0cbf33859bc82f
imNaNye <hyunkyungj0730@gmail.com>
2025-06-04 04:12:03 +0900

    [feat] 커뮤니티 댓글 db와 연결

--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,9 @@
+const authMiddleware = require('../middlewares/authMiddleware');
 const express = require('express');
 const router = express.Router();
 const commentsController = require('../controllers/commentsController');
 
 router.get('/:postId', commentsController.getComments);
-router.post('/:postId', commentsController.addComment);
+router.post('/:postId', authMiddleware, commentsController.addComment);
 
 module.exports = router;
