0b71987dbdca5f3ca594a305db145b08018cae47
jieun728 <qkekto1111@gmail.com>
2025-06-03 12:54:32 +0000

    popular books 작업 중간 저장

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { getTop4Books } = require('../controllers/popularController');
+
+router.get('/', async (req, res) => {
+  try {
+    const popularBooks = await getTop4Books();
+    console.log('[popularBooks]', popularBooks); // ← 확인용 로그
+    res.render('index', { popularBooks });
+  } catch (err) {
+    console.error('[메인페이지 인기 도서 에러]', err);
+    res.render('index', { popularBooks: [] });
+  }
+});
+
+module.exports = router;
+
