4cdae64da31d388c34d838f49758b45d1fa46b5c
disktus <lnsy0908@naver.com>
2025-06-05 20:18:02 +0900

    마이페이지 css 피그마 디자인으로 수정 및 백엔드 보완

--- controllers/bookReviewController.js
+++ controllers/bookReviewController.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-const mysql = require('mysql2/promise');
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: process.env.DB_PASSWORD,
-  database: 'bookmoa'
-});
-=======
 const db = require('../db');
->>>>>>> e53b9846de1247f217b4252222ab23d1e3c37b70
 
 // 리뷰 작성 폼 렌더링
 exports.reviewForm = (req, res) => {
@@ -24,28 +14,6 @@ exports.reviewForm = (req, res) => {
 // 리뷰 생성
 exports.createReview = async (req, res) => {
   const isbn13 = req.params.isbn13;
-<<<<<<< HEAD
-  const { nickname, comment } = req.body;
-  
-  try {
-    // 1. 먼저 book_id 조회
-    const [books] = await pool.query('SELECT book_id FROM book WHERE isbn = ?', [isbn13]);
-    if (books.length === 0) {
-      // 책이 없으면 먼저 book 테이블에 추가
-      const [result] = await pool.query(
-        'INSERT INTO book (isbn) VALUES (?)',
-        [isbn13]
-      );
-      var book_id = result.insertId;
-    } else {
-      var book_id = books[0].book_id;
-    }
-
-    // 2. 리뷰 저장
-    await pool.query(
-      'INSERT INTO book_review (user_id, book_id, content) VALUES (?, ?, ?)',
-      [nickname, book_id, comment]
-=======
   const { comment, rating } = req.body;
   const user_id = req.user?.user_id;
 
@@ -70,17 +38,11 @@ exports.createReview = async (req, res) => {
     await db.query(
       'INSERT INTO book_review (user_id, book_id, content, rating) VALUES (?, ?, ?, ?)',
       [user_id, book_id, comment, rating]
->>>>>>> e53b9846de1247f217b4252222ab23d1e3c37b70
     );
 
     res.redirect(`/book/${isbn13}`);
   } catch (error) {
-<<<<<<< HEAD
-    console.error('리뷰 저장 실패:', error);
-    res.status(500).send('리뷰 저장에 실패했습니다.');
-=======
     console.error('리뷰 저장 실패:', error); // 상세 로그 확인
     res.status(500).send('리뷰 등록에 실패했습니다.');
->>>>>>> e53b9846de1247f217b4252222ab23d1e3c37b70
   }
 };
