62591f1a774b74e6a9ca76d4b4a9d56356810edb
disktus <lnsy0908@naver.com>
2025-06-05 22:04:42 +0900

    작성한 글, 댓글 조회 구현중

--- routes/userContents.js
+++ routes/userContents.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const userContentsController = require('../controllers/userContentsController');
+const authenticateJWT = require('../middlewares/authMiddleware');
 
-// 유저가 작성한 도서 리뷰, 도서관 리뷰, 커뮤니티 게시글, 커뮤니티 댓글 조회
-router.get('/user/:user_id/book-reviews', userContentsController.getBookReviewsByUser);
-router.get('/user/:user_id/library-reviews', userContentsController.getLibraryReviewsByUser);
-router.get('/user/:user_id/community-posts', userContentsController.getCommunityPostsByUser);
-router.get('/user/:user_id/community-comments', userContentsController.getCommunityCommentsByUser);
+// 도서 리뷰
+router.get('/book-reviews', authenticateJWT, userContentsController.getBookReviewsByUser);
+router.patch('/book-reviews/:reviewId', authenticateJWT, userContentsController.updateBookReview);
+router.delete('/book-reviews/:reviewId', authenticateJWT, userContentsController.deleteBookReview);
+
+// 도서관 리뷰
+router.get('/library-reviews', authenticateJWT, userContentsController.getLibraryReviewsByUser);
+router.patch('/library-reviews/:reviewId', authenticateJWT, userContentsController.updateLibraryReview);
+router.delete('/library-reviews/:reviewId', authenticateJWT, userContentsController.deleteLibraryReview);
+
+// 커뮤니티 글 조회
+router.get('/community-posts', authenticateJWT, userContentsController.getCommunityPostsByUser);
+router.patch('/community-posts/:postId', authenticateJWT, userContentsController.updateCommunityPost);
+router.delete('/community-posts/:postId', authenticateJWT, userContentsController.deleteCommunityPost);
+
+// 커뮤니티 댓글
+router.get('/community-comments', authenticateJWT, userContentsController.getCommunityCommentsByUser);
+router.patch('/community-comments/:commentId', authenticateJWT, userContentsController.updateComment);
+router.delete('/community-comments/:commentId', authenticateJWT, userContentsController.deleteComment);
 
 module.exports = router;
